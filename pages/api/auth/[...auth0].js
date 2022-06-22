611e1dec1a989847cdb962e48328e0a40d0c41f0
Grant Tuttle <grant.tuttle@gmail.com>
2022-06-21 20:52:33 -0400

    Remove comments and dead code.

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,17 +1,3 @@
 import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 
-const getLoginState = (req, loginOptions) => {
-    return {
-        returnTo: `https://${req.headers.host}/profile`
-    }
-}
-
-export default handleAuth({
-    // async login (req, res) {
-    //     try {
-    //         await handleLogin(req,res, { getLoginState })
-    //     } catch (error) {
-    //         res.status(error.state || 500).end(error.message)
-    //     }
-    // }
-});
+export default handleAuth();
